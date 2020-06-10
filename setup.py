84c3c7de6e571563506cfb549ab1cc35722b1c2a
Gary T. Giesen <ggiesen@centrilogic.com>
2020-06-10 15:53:57 -0400

    Implement cli() method, make changes to setup.py in preparation for pushing to GitHub

--- setup.py
+++ setup.py
@@ -5,27 +5,29 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+__author__ = 'Gary T. Giesen <ggiesen@centrilogic.com>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-dellos6",
-    version="0.1.0",
+    version="0.0.1",
     packages=find_packages(),
-    author="David Barroso",
-    author_email="dbarrosop@dravetech.com",
-    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    author="Gary T. Giesen",
+    author_email="ggiesen@centrilogic.com",
+    description="NAPALM driver for Dell EMC Networking OS6 Operating System",
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
-         'Programming Language :: Python :: 2',
-         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
     ],
-    url="https://github.com/napalm-automation/napalm-dellos6",
+    url="https://github.com/ggiesen/napalm-dellos6",
     include_package_data=True,
     install_requires=reqs,
 )
